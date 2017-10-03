921d0f36af12296dcd2b67c389ec10c2cef7ace7
vonchuang <chuang8673@gmail.com>
2017-10-03 22:57:39 +0800

    add malloc exception handling

--- phonebook_opt.c
+++ phonebook_opt.c
@@ -1,4 +1,8 @@
 #include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include <assert.h>
 
 #include "phonebook_opt.h"
 
@@ -6,7 +10,7 @@
 entry *findLastName(char lastName[], entry *pHead)
 {
     /* TODO: implement */
-        while (pHead != NULL) {
+    while (pHead != NULL) {
         if (strcasecmp(lastName, pHead->lastName) == 0)
             return pHead;
         pHead = pHead->pNext;
@@ -19,6 +23,7 @@ entry *append(char lastName[], entry *e)
     /* TODO: implement */
     /* allocate memory for the new entry and put lastName */
     e->pNext = (entry *) malloc(sizeof(entry));
+    assert(e->pNext && "malloc for e->Next error.");
     e = e->pNext;
     strcpy(e->lastName, lastName);
     e->pNext = NULL;
